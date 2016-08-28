8a9373489a25ed189ce2df5a46e7b1c574b2e4f3
daviderixon <kontakt@daviderixon.se>
2016-08-28 16:03:06 +0200

    Added Alter button

--- album.php
+++ album.php
@@ -1,3 +1,24 @@
+<head>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link href='https://fonts.googleapis.com/css?family=Special+Elite' rel='stylesheet' type='text/css'>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>LPDB</title>
+
+  <!-- Bootstrap -->
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</head>
 <?php
 include 'DBConnect.php';
 $pageID = $_GET[pageID];
@@ -46,6 +67,6 @@ $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
         }
         print("</ol>");
         print ("</div>");
+        print("<button id="singlebutton" name="singlebutton" formaction="alter.php" class="btn btn-primary" type="submit" value="$UUID">Alter Information</button>")
     }
-
  ?>
